64c8e909cbcc20747908221ce2292e1c1283165c
Andrés Raúl Mateo Martínez <andresraul@gmail.com>
2019-11-18 19:53:17 +0100

    Mejoras en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+  fullImage: any;
+
+  photos = [
+    {
+      miniatura: '../../../assets/img/portfolio/thumbnails/1.jpg'
+    },
+    {
+      miniatura: '../../../assets/img/portfolio/thumbnails/2.jpg'
+    },
+    {
+      miniatura: '../../../assets/img/portfolio/thumbnails/3.jpg'
+    },
+    {
+      miniatura: '../../../assets/img/portfolio/thumbnails/4.jpg'
+    },
+    {
+      miniatura: '../../../assets/img/portfolio/thumbnails/5.jpg'
+    },
+    {
+      miniatura: '../../../assets/img/portfolio/thumbnails/6.jpg'
+    }
+  ]
+
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
+  }
+
+  openModal(template: TemplateRef<any>, image) {
+    this.modalRef = this.modalService.show(template);
+    this.fullImage = image;
+
   }
 
 }
