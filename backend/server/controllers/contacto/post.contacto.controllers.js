8347773c52b3a12f2498efa21fb15b0754b68d2d
Andrés Raúl Mateo Martínez <andresraul@gmail.com>
2019-11-21 17:17:01 +0100

    Agregado el módulo de sendGrid y sus configuraciones en el backend.

--- backend/server/controllers/contacto/post.contacto.controllers.js
+++ backend/server/controllers/contacto/post.contacto.controllers.js
@@ -1,3 +1,4 @@
+var helper = require('sendgrid').mail;
 const contactoCtrlsPost = {};
 
 contactoCtrlsPost.sendEmail = (req, res, next) => {
@@ -23,11 +24,37 @@ contactoCtrlsPost.sendEmail = (req, res, next) => {
         });
     }
 
-    res.json({
-        ok: true,
-        message: 'He recibido su mensaje, me comunicaré con usted lo más pronto posible. ¡Gracias por contactarme!'
-    })
 
+    var from_email = new helper.Email('andresraul@gmail.com');
+    var to_email = new helper.Email('portafolio@andresmateo.com');
+    var subject = `Desde el portafolio, correo de: ${name}`;
+    var content = new helper.Content('text/plain', `Correo de: ${email}. Cuerpo del correo: ${body}`);
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+    });
+
+    sg.API(request, function(error, response) {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'No hemos podido enviar su mensaje por problemas en el servidor. Si es posible, Inténtelo más tarde. Disculpe las molestias'
+                }
+            });
+        }
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+        res.json({
+            ok: true,
+            message: 'He recibido su mensaje y me comunicaré con usted lo más pronto posible. ¡Gracias por contactarme!'
+        })
+    });
 
 }
 
